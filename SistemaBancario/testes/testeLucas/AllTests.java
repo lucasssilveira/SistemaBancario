aac96c4d8d094e603fd6c8ea4246d08fc0eee76e
Lucas Silveira <lukas.silveira@hotmail.com>
2023-02-09 14:13:15 -0300

    Criação da classe suite que executa todos os testes

---
+++ SistemaBancario/testes/testeLucas/AllTests.java
@@ -0,0 +1,13 @@
+package testeLucas;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({GerClienteTest_1.class,GerClienteTest_2.class})
+
+
+public class AllTests {
+
+}
